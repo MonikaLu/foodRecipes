c1046b02a9c2881ea26ad9b88294360a1acb9b66
Monika <monika-luu@hotmail.com>
2021-10-06 23:03:32 +0200

    added some styling to the form #3

--- foodRecipes/src/main/ui/src/App.js
+++ foodRecipes/src/main/ui/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Recipe from './components/Recipe'
 
 function App() {
   return (
     <div className="App">
       <h1>Food Recipes</h1>
+        <Recipe />
     </div>
   );
 }
---
+++ foodRecipes/src/main/ui/src/components/Recipe.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Recipe = () =>  {
+    return(
+        <form className="recipeForm">
+            <div>
+
+            </div>
+            <input type={"text"}/>
+            <button>
+                Save
+            </button>
+
+        </form>
+    );
+}
+
+export default Recipe;
